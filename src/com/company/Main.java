92e22c8a6203f9dd6f47529d174ba77e996a8e91
Anna <annam.sihvonen@gmail.com>
2021-09-01 10:36:39 +0300

    added a comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // imperative programming homework 4
         int n = 1;
         while ((n*n*n + 91) % 47 != 0)
             n = n + 1;
